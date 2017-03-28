6a5eda540a28c0057404e683cfca0e20dcf248b6
Anna-Livia <annalivia.cardin@gmail.com>
2017-03-28 20:29:39 +0200

    Nodes now reflect recency category and post count also they can be dregged around

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var communauteJSON;
-var listOfTopics = [];
+var listOfNodes = [];
+var ListOfTopics = [];
 
 function preload() {
   var url = 'http://communaute.amontourdeprogrammer.fr/latest.json';
@@ -7,7 +8,7 @@ function preload() {
 }
 
 function setup() {
-	createCanvas(710, 400);
+	createCanvas(600, 600);
 	var ListOfTopics = communauteJSON.topic_list.topics;
 
   	for (var i = 0; i < ListOfTopics.length; i++) {
@@ -16,25 +17,42 @@ function setup() {
 		post_count = ListOfTopics[i].posts_count;
 		created_at = ListOfTopics[i].created_at;
 		last_posted_at = ListOfTopics[i].last_posted_at;
-		recency = recent(last_posted_at, year(), month(), day());
+		recency = 1/recent(last_posted_at, year(), month(), day());
 		category_id = ListOfTopics[i].category_id
-		listOfTopics.push(new Topic(topic_Name, topic_id, post_count, created_at, last_posted_at, category_id, recency))
+		listOfNodes.push(new Topic(topic_Name, topic_id, post_count, created_at, last_posted_at, category_id, recency))
 		console.log("New topic added");
 	}
+
   	  
 }
 
 function draw() {
 	background(255);
-  	for (var i=0; i<listOfTopics.length; i++) {
-	    listOfTopics[i].move();
-	    listOfTopics[i].display();
-	    for (var j=i+1; j<listOfTopics.length; j++) {
-	    	var distance = dist(listOfTopics[i].x,listOfTopics[i].y,listOfTopics[j].x,listOfTopics[j].y);
-	    	if (distance < 100){
-	        	linking (listOfTopics[i], listOfTopics[j], distance);
-	        }
+  	for (var i=0; i<listOfNodes.length; i++) {
+	    listOfNodes[i].move();
+	    for (var j=i+1; j<listOfNodes.length; j++) {
+	    	var distance = dist(listOfNodes[i].x,listOfNodes[i].y,listOfNodes[j].x,listOfNodes[j].y);
+	    	//if (distance < 100){
+	        	//linking (listOfNodes[i], listOfNodes[j], distance);
+	        //}
 	    }
+	    listOfNodes[i].display();
+	}
+}
+function mouseDragged() {
+	for (var i=0; i<listOfNodes.length; i++) {
+		node = listOfNodes[i];
+		if (dist(mouseX, mouseY, node.x, node.y) < node.diameter) {
+			node.x = mouseX;
+			node.y = mouseY;
+		}
+		for (var j=1; j<listOfNodes.length; j++){
+			altNode = listOfNodes[j];
+			if (altNode.category_id == node.category_id){
+				distance = dist(node.x,node.y, altNode.x, altNode.y);
+				linking(node, altNode, distance);
+			}
+		}
 	}
 }
 
@@ -49,36 +67,39 @@ function Topic(temptopic_Name, temptopic_id, temppost_count, tempcreated_at, tem
 	this.recent = temprecent;
 
 
-	this.x = random(width);
-  	this.y = random(height);
-  	this.diameter = random(10, 30);
-  	this.xspeed = random(1,2);
- 	this.yspeed = random(1,2);
+	this.x = random(width/3)+ width/3;
+  	this.y = random(height/3) + height/3;
+  	this.diameter = 6*temppost_count;
+
+  	this.xspeed = random([-1, 1])*temprecent;
+ 	this.yspeed = random([-1, 1])*temprecent;
 
   	this.move = function() {
 	    this.x += this.xspeed;
 	    this.y += this.yspeed;
 	    
-	    if (this.x > width){ 
+	    if (this.x + this.diameter/2 > width){ 
 	      this.xspeed *= -1;
-	      this.x = width;
+	      this.x = width - this.diameter/2;
 	    }
-	    if (this.x < 0){ 
+	    if (this.x - this.diameter/2 < 0){ 
 	      this.xspeed *= -1;
-	      this.x = 0;
+	      this.x  = 0 + this.diameter/2;
 	    }
-	    if (this.y > height){ 
+	    if (this.y + this.diameter/2> height){ 
 	      this.yspeed *= -1;
-	      this.y = height;
+	      this.y = height - this.diameter/2;
 	    }
-	    if (this.y < 0){ 
+	    if (this.y  - this.diameter/2 < 0){ 
 	      this.yspeed *= -1;
-	      this.y = 0;
+	      this.y = 0+ this.diameter/2;
 	    }
 	};
 
 	this.display = function() {
-   		strokeWeight(1);
+   		strokeWeight(0.1);
+   		colorMode(HSB, 15);
+   		fill(this.category_id, 15, 10);
     	ellipse(this.x, this.y, this.diameter, this.diameter);
     };
 
@@ -99,7 +120,8 @@ function recent(dateAsString, yearNow, monthNow, dayNow) {
 }
 
 function linking(n, m, distance){
-	stroke(255,0,0);
-	strokeWeight(map(distance, 0, 100, 3, 0));
+	colorMode(RGB, 255);
+	stroke(10);
+	strokeWeight(map(distance, 0, 100, 0.3, 0));
 	line(n.x, n.y, m.x, m.y);
 }
